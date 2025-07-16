de0bca027d0d4b0974ff20a886a79885954d5d7a
Daniel Dawda <danieldawda2448@gmail.com>
2025-07-16 16:18:38 -0700

    game.py help funcs, client inputs, and server recv

--- server.py
+++ server.py
@@ -1,13 +1,15 @@
 import socket
 from threading import Thread
 from threading import Lock
+from game import updatePositions
+import time
 
 sock = socket.socket(
     socket.AF_INET,
     socket.SOCK_STREAM)
 
 #server's IP/port. Change as needed, should be part of client UI to choose right port/IP.
-sock.bind(("", 53333))
+sock.bind(("127.0.0.1", 53334))
 
 mutex = Lock() #general mutex lock for player state changes (new inputs, changing connectionsList, etc)
 #### Mutex locked variables
@@ -16,8 +18,15 @@ clientThreads = []
 gameStarted = False
 
 #North, West, South, East movement vectors for each player
-playerInputs = [[False, False, False, False]*4]
-gameState = []
+playerInputs = [[False, False, False, False],
+                [False, False, False, False],
+                [False, False, False, False],
+                [False, False, False, False]]
+# itinital game state dict
+gameState = {'pos': [[100, 100],
+                    [200, 100],
+                    [100, 200],
+                    [200, 200]]}
 
 ### end Mutex locked variables
 
@@ -25,6 +34,7 @@ gameState = []
 
 #handle all inputs from players, outputs will be handled in bulk via the broadcastGame() function
 def handleConnection(connection, index):
+    print("test")
     global gameStarted
     while True:
         with mutex:
@@ -43,8 +53,20 @@ def handleConnection(connection, index):
 
 #function to send the current game state to all players
 def broadcastGameUpdates():
-    pass
-    #todo
+    while True:
+        for conn in connectionList:
+            # format data ("POSXXXYYYXXXYYYXXXYYYXXXYYY")
+            data = ("POS"+str(gameState["pos"][0][0]).zfill(3)+
+                            str(gameState["pos"][0][1]).zfill(3)+
+                            str(gameState["pos"][1][0]).zfill(3)+
+                            str(gameState["pos"][1][1]).zfill(3)+
+                            str(gameState["pos"][2][0]).zfill(3)+
+                            str(gameState["pos"][2][1]).zfill(3)+
+                            str(gameState["pos"][3][0]).zfill(3)+
+                            str(gameState["pos"][3][1]).zfill(3))
+            print(data)
+            conn.send(data.encode())
+            time.sleep(1)
 
 #Fill the connectionsList, and create threads for them
 def getInitPlayers():
@@ -111,6 +133,51 @@ def getInitPlayers():
         
     return
 
+def initInputs():
+        
+        # launch a thread for each clients movement
+        for conn in connectionList:
+            Thread(target = inputs, args=(conn,)).start()
+
+def inputs(conn):
+
+    idx = connectionList.index(conn)
+    while True:
+        data = conn.recv(32).decode()
+        with mutex:
+            # if recv MOV data then mov based on dir
+            if "MOV" in data:
+                # print("START")
+                if "up" in data:
+                    playerInputs[idx][0] = True
+                elif "left" in data:
+                    playerInputs[idx][1] = True
+                elif "down" in data:
+                    playerInputs[idx][2] = True
+                elif "right" in data:
+                    playerInputs[idx][3] = True
+                
+            elif "STOP" in data:
+                # print("STOP")
+                if "up" in data:
+                    playerInputs[idx][0] = False
+                elif "left" in data:
+                    playerInputs[idx][1] = False
+                elif "down" in data:
+                    playerInputs[idx][2] = False
+                elif "right" in data:
+                    playerInputs[idx][3] = False
+
+                
+                # print(playerInputs)
+
+def updateGameState():
+
+    while True:
+        gameState['pos'] = updatePositions(playerInputs, gameState['pos'])
+        time.sleep(0.5)
+
+
 
 
 def main():
@@ -121,8 +188,10 @@ def main():
         getInitPlayers()
         
         #new inputs come from threads, mutex game logic
+        initInputs()
         
         #run game logic
+        Thread(target = updateGameState, args=()).start()
 
         #send new game state
         broadcastGameUpdates()
