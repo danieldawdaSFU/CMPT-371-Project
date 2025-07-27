fd056abcdbb0b789b4e9e2a68535e84d7c15b35d
reh13597 <alexlguo18@gmail.com>
2025-07-27 14:54:11 -0700

    Uncommented call to draw_game_over and changed first level tile time limit

--- server.py
+++ server.py
@@ -306,7 +306,7 @@ def getDifficulty():
     elif currentScore >= 8:
         return 2, 3, 15
     else:
-        return 1, 2, 60
+        return 1, 2, 20
 
 # Creates the given number of goals for each player which start with the given time limit
 def generateGoals():
